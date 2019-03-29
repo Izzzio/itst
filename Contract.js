f69338a5e7063cf4e86fa2d4bdf863f69215200c
Dmitriy Elsukov <demndik@yandex.ru>
2019-03-30 00:53:02 +0300

    updates

--- Contract.js
+++ Contract.js
@@ -1,51 +1,77 @@
-'use strict';
+/**
+ iZ³ | Izzzio blockchain - https://izzz.io
 
-class Contract {
-    constructor() {
-        this._activeContracts = new Map();
-        this._completedContracts = new Map();
+ Copyright 2018 Izio Ltd (OOO "Изио")
 
-        let contractIdentifier = {perfomerId: "Вася", customerId: "Петя", contractId: "1"};
-        var perfomerInfo = {fName: 'Вася', lName: 'Васечкин'};
-        var customerInfo = {fName: 'Петя', lName: 'Петячкин'};
-        var infoFromOrderCONSTANT = {};
-        var infoFromOrderEDIT = {};
-        var infoFromOfferCONSTANT = {};
-        var infoFromOfferEDIT = {};
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-        this._activeContracts.set(
-            contractIdentifier,
-            {
-                perfomerInfo,
-                customerInfo,
-                infoFromOrderCONSTANT,
-                infoFromOrderEDIT,
-                infoFromOfferCONSTANT,
-                infoFromOfferEDIT
-            }
-        );
-        //console.log(this._activeContracts.get(contractIdentifier));
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+const CONTRACT_OWNER = '-----BEGIN RSA PUBLIC KEY-----\n' +
+    'MIIBCgKCAQEApSJ2Lm6h26vHgiqB4VcyOZE+meRB6Jaow6Z+6cBn43fvcM57l8O2DfFTgo9R\n' +
+    '4AUavuFJU8bekhcCWYC53RErumjHBrWVviGDOxRALfev8fOU6V+hm9E7FGiW5RXMew5729lt\n' +
+    'rOxsyrayUtBLsd6BAEO5n/AtAI08Et403X/UX/7N/9zKu+F2E/fi1VlvJS07TtgPoRuT9vx6\n' +
+    'ol7B0OcqGU0lIe84TasfX4pN9RIZe3+O8idBTg9aHbtuD2qSSQ9x1jpcD4wOtb+FhgBJ3dOI\n' +
+    'eIC3eapWvK4HFtAtX1uIyod3LruSVssrNtLEqWOgD5MwOlv1KWAR0ZDZ3cYNL8Of8QIDAQAB\n' +
+    '-----END RSA PUBLIC KEY-----\n';
+
+/**
+ * Factory contract
+ */
+class factoryContract extends Contract {
+
+    init() {
+        super.init();
+        this._perfomerList = new BlockchainMap('_perfomerList');
+        this._customerList = new BlockchainMap('_customerList');
+        this._activeContracts = new BlockchainMap('_activeContracts');
+        this._completedContracts = new BlockchainMap('_completedContracts');
     }
 
-    getActiveContract(perfomerId, customerId, contractId)
-    {
-        let contractIdentifier = {perfomerId: perfomerId, customerId: customerId, contractId: contractId};
+    static _createContractKey(perfomerId, customerId, contractId) {
+        return perfomerId + '_' + customerId + '_' + contractId;
+    }
 
-        //console.log(this._activeContracts);
-        //console.log(contractIdentifier);
+    getActiveContracts(perfomerId, customerId, contractId) {
+        let contractKey = this._createContractKey(perfomerId, customerId, contractId);
+        assert.assert(
+            this._activeContracts[contractKey] !== 0,
+            "Contract with this parameters doesn't exist or already completed");
+        return this._activeContracts[contractKey];
+    }
 
-        if(!this._activeContracts.has(contractIdentifier)){
-            return "Contract with this parameters doesn't exist or already completed";
-        }
-        return this._activeContracts.get(contractIdentifier);
+    getCompletedContracts(perfomerId, customerId, contractId) {
+        let contractKey = this._createContractKey(perfomerId, customerId, contractId);
+        assert.assert(
+            this._completedContracts[contractKey] !== 0,
+            "Contract with this parameters doesn't exist or isn't yet complete");
+        return this._completedContracts[contractKey];
+    }
 
-        //console.log(contract);
+    _addPerfomer(perfomerId, perfomerInfo) {
+        this._perfomerList[perfomerId] = perfomerInfo;
+    }
 
-        //require (activeContracts[perfomerId][customerId][contractId] != 0, "Contract with this parameters doesn't exist or already completed");
-        //return activeContracts[perfomerId][customerId][contractId];
+    removePerfomer(perfomerId) {
+        delete this._perfomerList[perfomerId];
+    }
+
+    _addCustomer(customerId, customerInfo) {
+        this._customerList[customerId] = customerInfo;
+    }
+
+    removeCustomer(customerId) {
+        delete this._customerList[customerId];
     }
 }
 
-const factory = new Contract();
-let contract = factory.getActiveContract("Вася", "Петя", "1");
-console.log(contract);
+global.registerContract(factoryContract);
